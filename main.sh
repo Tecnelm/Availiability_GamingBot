ae084b638fc7cb23ce77da3dcdc0f129a975e8dd
Tecnelm <cleman91@gmail.com>
2022-05-01 20:30:29 +0200

    update main

--- main.sh
+++ main.sh
@@ -5,6 +5,7 @@ sed -i 's/$token/'$TOKEN'/' ./config.yml
 sed -i 's/$timeMinAFK/'$TIME_MIN_AFK'/' ./config.yml 
 sed -i 's/$mac/'$MAC'/' ./config.yml 
 sed -i 's/$port/'$PORT'/' ./config.yml
+sed -i 's/$address/'$ADDRESS'/' ./config.yml
 
 python3 main.py
 
